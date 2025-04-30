36424b18167d141c8769f441be8e74e3aa081ef1
Nguyen Huu Duc <nguyenhuuduc458@gmail.com>
2025-04-30 13:29:46 +0700

    add crashlytic

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.google.service) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
